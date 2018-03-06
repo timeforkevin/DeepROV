4ec7542103b2f311f5f488615c02cd0501345477
Anish Walawalkar <aswalawa@uwaterloo.ca>
2018-03-06 15:09:22 -0500

    added feature recognision while streaming live video

--- Pi/video_streaming/camera.py
+++ Pi/video_streaming/camera.py
@@ -1,22 +1,104 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from imutils.video import VideoStream
+from imutils.video import FPS
+import imutils
+import time
 import cv2
+import numpy as np
+
 
 class VideoCamera(object):
+
     def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
-        self.video = cv2.VideoCapture(0)
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        # self.video = cv2.VideoCapture('video.mp4')
-    
+
+        self.vs = VideoStream(src=0).start()
+        self.fps = FPS().start()
+        self.classes = self.classes = [
+            'background',
+            'aeroplane',
+            'bicycle',
+            'bird',
+            'boat',
+            'bottle',
+            'bus',
+            'car',
+            'cat',
+            'chair',
+            'cow',
+            'diningtable',
+            'dog',
+            'horse',
+            'motorbike',
+            'person',
+            'pottedplant',
+            'sheep',
+            'sofa',
+            'train',
+            'tvmonitor',
+            ]
+        self.colors = np.random.uniform(0, 255,
+                size=(len(self.classes), 3))
+        self.net = \
+            cv2.dnn.readNetFromCaffe('MobileNetSSD_deploy.prototxt.txt'
+                , 'MobileNetSSD_deploy.caffemodel')
+        self.confidence = 0.2
+
     def __del__(self):
-        self.video.release()
-    
+        self.fps.stop()
+        self.vs.stop()
+
     def get_frame(self):
-        success, image = self.video.read()
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
-        ret, jpeg = cv2.imencode('.jpg', image)
+        frame = self.vs.read()
+        frame = imutils.resize(frame, width=1280)
+
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (1280, 1024)),
+                0.007843, (300, 300), 127.5)
+
+        self.net.setInput(blob)
+        detections = self.net.forward()
+
+        # loop over the detections
+
+        for i in np.arange(0, detections.shape[2]):
+
+            # extract the confidence (i.e., probability) associated with
+            # the prediction
+
+            confidence = detections[0, 0, i, 2]
+
+            # filter out weak detections by ensuring the `confidence` is
+            # greater than the minimum confidence
+
+            if confidence > self.confidence:
+
+                # extract the index of the class label from the
+                # `detections`, then compute the (x, y)-coordinates of
+                # the bounding box for the object
+
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype('int')
+
+                # draw the prediction on the frame
+
+                label = '{}: {:.2f}%'.format(self.classes[idx],
+                        confidence * 100)
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                              self.colors[idx], 2)
+                y = (startY - 15 if startY - 15 > 15 else startY + 15)
+                cv2.putText(
+                    frame,
+                    label,
+                    (startX, y),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    self.colors[idx],
+                    2,
+                    )
+
+        self.fps.update()
+
+        (ret, jpeg) = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
