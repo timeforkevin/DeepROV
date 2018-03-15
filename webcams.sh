0bf9d2ac275ee0561fa366cd5b687abb2809d36e
timeforkevin <kevinjin@live.ca>
2018-03-15 13:32:10 -0400

    start webcam streaming in background

--- webcams.sh
+++ webcams.sh
@@ -3,8 +3,8 @@
 v4l2-ctl --device=/dev/video0 --set-fmt-video=width=800,height=600,pixelformat=1
 v4l2-ctl --device=/dev/video1 --set-fmt-video=width=800,height=600,pixelformat=1
 
-cvlc --sout=#rtp{sdp=rtsp://192.168.0.100:8080} 'v4l2:///dev/video0:chroma=H264:width=800:height=600:fps=30'
-cvlc --sout=#rtp{sdp=rtsp://192.168.0.100:8081} 'v4l2:///dev/video1:chroma=H264:width=800:height=600:fps=30'
+cvlc --sout=#rtp{sdp=rtsp://192.168.0.100:8080} 'v4l2:///dev/video0:chroma=H264:width=800:height=600:fps=30' &
+cvlc --sout=#rtp{sdp=rtsp://192.168.0.100:8081} 'v4l2:///dev/video1:chroma=H264:width=800:height=600:fps=30' &
 
 # cvlc v4l2:///dev/video0:chroma=h264:width=800:height=600 --sout '#standard{access=http,mux=ts,dst=192.168.0.100:8080,name=stream,mime=video/ts}' &
 # cvlc v4l2:///dev/video1:chroma=h264:width=800:height=600 --sout '#standard{access=http,mux=ts,dst=192.168.0.100:8081,name=stream,mime=video/ts}' &
